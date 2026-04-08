ce2ee162c7a24810467419be52ff078a51895d46
MuhammadAbdullah-coder997 <abdullahiqbal2008@gmail.com>
2026-04-08 17:13:30 +0500

    chore: import hero component onto homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Navbar from "@/components/Navbar"
+import Hero from "@/components/Hero"
 
 export default function HomePage() {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen bg-[#F2E7C9]">
       <Navbar />
+      <div className="pt-24">
+        <Hero />
+      </div>
     </div>
   );
 }
